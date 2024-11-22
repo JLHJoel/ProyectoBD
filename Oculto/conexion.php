4c655d1faf115523519a84521665d1e0d2adf8ec
JLHJoel <lozano.joel@uabc.edu.mx>
2024-11-22 14:28:48 -0800

    Add connection file to the DB

--- Oculto/conexion.php
+++ Oculto/conexion.php
@@ -1,9 +1,9 @@
 <?php
 
-$host_name = "sql205.infinityfree.com";
-$username = "if0_37758289";
-$password = "yHtrfdHJn1HlDU";
-$db_name = "if0_37758289_resenas_db";
+$host_name = "localhost"; //Aqui agrega el hostname de infinityfree
+$username = "root";   //el username de la DB de infinityfree
+$password = "1234"; //el password de la DB de infinityfree
+$db_name = "resenas_db"; //el nombre de la BD de infinityfree
 
 $conn = new mysqli($host_name, $username, $password, $db_name);
 
